b2ff4008e40430f14ab1feea77059c2cde7f96e5
Adrian Siekierka <kontakt@asie.pl>
2026-02-16 19:09:59 +0100

    meson: rename executable back to zeta86

--- res/macos/zeta.sh
+++ res/macos/zeta.sh
@@ -1,5 +1,9 @@
-#!/bin/sh
+#!/bin/bash
 EXEC_DIR="$(dirname "$0")"
-cd "$EXEC_DIR"/../../..
+if [ -f "$EXEC_DIR"/ZZT.EXE ] || [ -f "$EXEC_DIR"/SUPERZ.EXE ]; then
+	cd "$EXEC_DIR"
+else
+	cd "$EXEC_DIR"/../../..
+fi
 SCRIPT_DIR=$(pwd)
-"$EXEC_DIR"/zeta "$@"
+"$EXEC_DIR"/zeta86 "$@"
